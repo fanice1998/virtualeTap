da58590aaf6c807a76ee9606ab39cb2d5afbdec2
fanice <fanice0721@gmail.com>
2024-11-23 08:01:26 +0800

    buid home page

--- app/src/main/java/com/example/virtualtap/MainActivity.kt
+++ app/src/main/java/com/example/virtualtap/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.example.virtualtap
 
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.virtualtap.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
